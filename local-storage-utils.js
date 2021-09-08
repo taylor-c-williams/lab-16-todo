22fd25a5a75d27dae799844f304b249afbb87649
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-08 12:18:34 -0700

    inital utils

---
+++ local-storage-utils.js
@@ -0,0 +1,42 @@
+export const USER = 'USER';
+
+export function getUser(){
+    const stringyUser = localStorage.getItem(USER);
+    return JSON.parse(stringyUser);
+}
+
+export function setUser(user){
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem(USER, stringyUser);
+}
+
+export function getTasks(){
+    const { tasks } = getUser();
+    return tasks;
+}
+
+export function setTasks(tasks){
+    const user = getUser();
+    user.tasks = tasks;
+    setUser(user);
+}
+
+export function addTask(message){
+    const tasks = getTasks();
+    const newTask = {
+        id: Math.floor(math.random()* 10000),
+        task: message,
+        completed: false,
+    };
+
+    tasks.push(newTask);
+    setTasks(tasks);
+}
+
+export function completeTask(id){
+    const tasks = getTasks();
+    const taskMatch = tasks.find(task => task.id === id);
+    taskMatch.completed = true;
+    setTasks(tasks);
+}
+
