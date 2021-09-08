183ae7f498818c20f24f3c1e7918538df57160e3
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-08 14:11:16 -0700

    half of a working event listener

--- app.js
+++ app.js
@@ -3,3 +3,40 @@
 
 // const data = new URLSearchParams(window.location.search);
 // const questId = data.get('id')
+
+import { getUser } from "./local-storage-utils.js";
+
+const loginEl = document.getElementById('login');
+const usernameInput = document.getElementById('username-input');
+const passInput = document.getElementById('pass-input');
+
+loginEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const loginData = new FormData(loginEl);
+  const user = getUser();
+  const userName = user.find(user => user.username === usernameInput);
+  const userPass = user.find(user => user.password === passInput);
+
+  if (userPass === user.password && userName === user.username) 
+  {
+    window.location = `./todos/index.html?username=${userName}`;
+  }
+    
+  })
+  
+  // const searchParams = new URLSearchParams(window.location.search);
+  // const userID = searchParams.get('id')
+
+
+//   export function completeTodo(id) {
+//     // - find the correct todo in your array
+//     const todos = getTodos();
+
+//     const matchingTodo = todos.find(todo => todo.id === id);
+
+//     matchingTodo.completed = true;
+//     // - set that todo to `completed: true`
+//     // save todos
+
+//     setTodos(todos);
+// 
---
+++ data-model.js
@@ -0,0 +1,21 @@
+{
+    username: 'planty12',
+    password: 1234,
+    todos: [
+        {
+            id: 2343,
+            todo: 'water the garden',
+            completed: false
+        },
+        {
+            id: 3827,
+            todo: 'feed the dog',
+            completed: false
+        },
+        {
+            id: 4927,
+            todo: 'walk the dog',
+            completed: true
+        },
+    ]
+}
