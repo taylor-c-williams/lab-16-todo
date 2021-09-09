5983a7b004fd99bf916db2772ac8dfcc5df6c17f
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-09 13:35:05 -0700

    Formatting, making sure to save before a break

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,39 +1,32 @@
-import { getUser, getTasks, addTask } from '../local-storage-utils.js';
+import { getUser, getTasks, addTask } from "../local-storage-utils.js";
 
-
-const header = document.getElementById('header');
-const taskList = document.getElementById('task-list');
+const header = document.getElementById("header");
+const taskList = document.getElementById("task-list");
 const user = getUser();
 const username = user.username;
 const tasks = getTasks();
-const taskItem = document.getElementById('new-task-item');
+const taskItem = document.getElementById("new-task-item");
 
-const newTaskEl = document.getElementById('newTask');
-newTaskEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const newTaskData = new FormData(newTaskEl);
-  const newTask = newTaskData.get('new-task');
-  
-  if (taskItem.value.length < 1) return;
-  addTask(taskItem.value);
-	// Add item to wishlist
-	taskList.innerHTML += '<li>' + taskItem.value + '</li>';
+const newTaskEl = document.getElementById("newTask");
+newTaskEl.addEventListener(
+  "submit",
+  (e) => {
+    e.preventDefault();
+    const newTaskData = new FormData(newTaskEl);
+    const newTask = newTaskData.get("new-task");
+    if (taskItem.value.length < 1) return;
+    addTask(taskItem.value);
+    taskList.innerHTML += "<li>" + taskItem.value + "</li>";
+    taskItem.value = "";
+  },
+  false
+);
 
-	// Clear input
-taskItem.value = '';
-
-}, false);
-
-  
-
-
-tasks.forEach(task => console.log(task.task));
+tasks.forEach((task) => console.log(task.task));
 let allTasks = [];
 for (let i = 0; i < tasks.length; i++) {
-allTasks.push([tasks[i].task])};
-
+  allTasks.push([tasks[i].task]);
+}
 
 header.textContent = `${username}'s To Do List!`;
 taskList.textContent = allTasks;
-
-
