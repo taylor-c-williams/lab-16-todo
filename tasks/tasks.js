59d7fbfc6aff7ca686eb457349c9431ae90de2bc
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-11 10:59:51 -0700

    pull merge

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -6,6 +6,7 @@ const user = getUser();
 const username = user.username;
 const tasks = getTasks();
 const taskItem = document.getElementById("new-task-item");
+const checked = document.getElementById("checkbox");
 
 
 const newTaskEl = document.getElementById("newTask");
@@ -15,9 +16,11 @@ newTaskEl.addEventListener(
     e.preventDefault();
     if (taskItem.value.length < 1) return;
     addTask(taskItem.value);
-    taskList.innerHTML += "<li>" + taskItem.value + "</li>";
+    taskList.innerHTML += "<li>" + taskItem.value + "</li>" + "<input type=\"checkbox\"id=\"checkbox\">";
     taskItem.value = "";
-  },
+  }
+
+  
 );
 
 let allTasks = [];
@@ -26,8 +29,30 @@ for (let i = 0; i < tasks.length; i++) {
   allTasks.push([tasks[i].task]);
 }
   for (let i = 0; i < allTasks.length; i++) {
-  taskList.innerHTML += "<li>" + allTasks[i] + "</li>";
-  }
+    // if(allTasks[i].checked){
+    //   completed = true;
+  //   // }
+  // taskList.innerHTML += "<li>" + allTasks[i] + "</li>" + "<input type=\"checkbox\" id=\"checkbox\">";
+  
+
+  let taskList = document.querySelector('#task-list');
+    
+  let tasksFinal = allTasks.map(task => {
+      let li = document.createElement('li');
+      li.textContent = task;
+     
+      return li;
+  });
+
+
+  let newCheckBox = document.createElement('input');
+  newCheckBox.type = 'checkbox';
+  newCheckBox.id = 'checkbox';
+  
+  taskList.append(...tasksFinal, newCheckBox);
+
+
+}
 
 header.textContent = `${username}'s To Do List!`;
 
